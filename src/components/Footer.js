89a818bc4575d00ce50b20d49e53f710131b04d6
Romeo De Timmerman <romeo.detimmerman@gmail.com>
2025-01-21 11:18:18 +0100

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from "react";
 import contentData from "../content.json";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCodeFork, faStar } from "@fortawesome/free-solid-svg-icons";
 
 /* ==========================================
 *   JSON Template Example
@@ -19,29 +16,8 @@ import { faCodeFork, faStar } from "@fortawesome/free-solid-svg-icons";
 
 */
 
-async function fetchRepoData(repoApiLink) {
-  const response = await fetch(repoApiLink);
-  const data = await response.json();
-  return data;
-}
-
 const Footer = () => {
   const general = contentData.general;
-  const repoLink = contentData.repo_stats.repo_link;
-  const repoApiLink = contentData.repo_stats.api_link;
-
-  const iconStyle = {
-    fontSize: "0.8rem", // Adjust the size as needed
-    marginRight: "1.2rem", // Add space between icon and text
-  };
-
-  const [repoData, setRepoData] = useState(null);
-
-  useEffect(() => {
-    fetchRepoData(repoApiLink).then((data) => {
-      setRepoData(data);
-    });
-  }, [repoApiLink]);
 
   return (
     <footer>
